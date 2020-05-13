68c26194b030754e18af7a60868c19d9fb91c82e
Kolch <alex-k99@mail.ru>
2020-05-13 17:12:43 +0300

    test comment added

--- Helper/Screens/MainPage/MainPageViewController.swift
+++ Helper/Screens/MainPage/MainPageViewController.swift
@@ -12,8 +12,9 @@ class MainPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        // TEEEEST
     }
 
 
