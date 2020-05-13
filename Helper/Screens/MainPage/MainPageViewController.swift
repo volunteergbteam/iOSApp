a8565f1e3c4bd6d7b7896d4dc70e430c535a6048
Kolch <alex-k99@mail.ru>
2020-05-13 16:38:23 +0300

    main screen vc creared

---
+++ Helper/Screens/MainPage/MainPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainPageViewController.swift
+//  Helper
+//
+//  Created by Kolchedantsev Alexey on 13.05.2020.
+//  Copyright © 2020 vTeam. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
